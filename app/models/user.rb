2fd59c7083de28e0c40b84fcc941a0bdd629052e
Ewetumo Alexander <trinoxf@gmail.com>
2010-10-18 15:53:26 +0100

    7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 
+validates_uniqueness_of :login
   attr_accessible :login, :email, :password,:password_confirmation
   acts_as_authentic do |c|
    c.validate_email_field = true
